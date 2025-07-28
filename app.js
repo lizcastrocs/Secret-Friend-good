d22b8eb7f1f9b80d2b0af2884652894f1c0a22a1
lizcastrocs <lizcastrocs@gmail.com>
2025-07-28 09:27:54 +0200

    Update app.js

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+console.log("ejecutando el sistema")
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
 const listaDeAmigos = [];
 const listaDeGanadores = [];
